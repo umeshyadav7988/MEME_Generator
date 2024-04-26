ceacf877da6c0852a8713cf0dbcb8f198e08d23c
Umesh Rao <umeshyadav7988@gmail.com>
2024-04-27 01:29:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const generateMemeBtn=document.querySelector(".meme-generator .generate-meme-btn");
+const memeImage=document.querySelector(".meme-generator img");
+const memeTitle=document.querySelector(".meme-generate .meme-title");
+const memeAuthor=document.querySelector(".meme-generator .meme-author");
+
+const updateDetails=(url,title,author)=>{
+    memeImage.setAttribute("src",url);
+    memeTitle.innerHTML=title;
+    memeAuthor.innerHTML=author;
+}
+
+const generateMeme=()=>{
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((Response)=>Response.json())
+    .then(data=>{
+            updateDetails(data.url,data.title,data.author);
+    });
+};
+
+
+generateMemeBtn.addEventListener("click",generateMeme)
